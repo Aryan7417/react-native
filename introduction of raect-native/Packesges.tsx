f5942011a27d59fc75d4d09576190d6569b9c6e0
Aryan Yadav <caryan830@gmail.com>
2026-07-02 03:37:10 +0530

    instral packeae and webView

---
+++ introduction of raect-native/Packesges.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {WebView} from 'react-native-webview'
+
+const Packesges = () => {
+  return (
+    <WebView source={uri:"https://www.npmjs.com/package/react-native"}/>
+  )
+}
+
+export default Packesges
--- introduction of raect-native/componet.tsx
+++ introduction of raect-native/componet.tsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import { View,Text } from 'react-native' 
-//import ButtonOnPress from './ButtonOnPress.jsx'
-//import Props from './Props.tsx'
+// import React from 'react'
+// import { View,Text } from 'react-native' 
+// //import ButtonOnPress from './ButtonOnPress.jsx'
+// //import Props from './Props.tsx'
 
-//import Hellostyle from './hellostyle.jsx'
-//import InputVAlue from './inputVAlue'
-//import SimpleForm from './SimpleForm'
-//import ListAndFlatList from './ListAndFlatList.jsx'
-//import Map from '../introduction of raect-native/Map.jsx'
-//import Grid from './grid.jsx'
-//import Flatlist from './Flatlist.jsx' 
-//import Sectionlist from './Sectionlist.jsx'
-//import ClasssCOmp from './ClasssCOmp.jsx'
-//import Hooks from './Hooks.tsx'
-//import HideComp from './HideComp.tsx'
-//import CompUnmount from './CompUnmount.tsx'
-///import STylessf from './STyless.tsx'
-// import Radio from './Radio.tsx'
-//import Loader from './Loader.tsx'
-//import Moodal from './Moodal.tsx'
-//import LongPress from './LongPress.tsx'
-// import Statusbar from './StatusBar.tsx'  
-import Platforms from './Platforms.tsx'
-
-
+// //import Hellostyle from './hellostyle.jsx'
+// //import InputVAlue from './inputVAlue'
+// //import SimpleForm from './SimpleForm'
+// //import ListAndFlatList from './ListAndFlatList.jsx'
+// //import Map from '../introduction of raect-native/Map.jsx'
+// //import Grid from './grid.jsx'
+// //import Flatlist from './Flatlist.jsx' 
+// //import Sectionlist from './Sectionlist.jsx'
+// //import ClasssCOmp from './ClasssCOmp.jsx'
+// //import Hooks from './Hooks.tsx'
+// //import HideComp from './HideComp.tsx'
+// //import CompUnmount from './CompUnmount.tsx'
+// ///import STylessf from './STyless.tsx'
+// // import Radio from './Radio.tsx'
+// //import Loader from './Loader.tsx'
+// //import Moodal from './Moodal.tsx'
+// //import LongPress from './LongPress.tsx'
+// // import Statusbar from './StatusBar.tsx'  
+// import Packesges from './Packesges'
 
 
 
-const componet = () => {
-  return (
-    <View style={{backgroundColor:'#C2B551'}}>
-        <Text style={{fontSize:23 ,padding:100,color:"red"}}>------Testing the code--------</Text>
-        <Platforms/>
+
+
+// const componet = () => {
+//   return (
+//     <View style={{backgroundColor:'#C2B551'}}>
+//         <Text style={{fontSize:23 ,padding:100,color:"red"}}>------Testing the code--------</Text>
+//         <Packesges/>
         
 
         
@@ -41,53 +41,63 @@ const componet = () => {
         
         
   
-    </View>
+//     </View>
 
+//   )
+
+// }
+
+
+
+// // const Appdata=()=>{
+// //     return(
+// //         <View>
+// //             <Text style={{fontSize:30, color:'red'}}>Name:Anil</Text>
+// //             <Text style={{fontSize:30}}>Age:34</Text>
+// //             <Text style={{fontSize:30}}>Email:aryan89@gamil.com</Text>
+// //         </View>
+// //     )
+
+// // }
+
+// // const CompneyNAme=()=>{
+// //     return(
+
+// //         <View>
+// //         <Text style={{
+// //             fontSize:34
+// //         }}>compney Name:xyz compney</Text>
+
+
+// //         <Text style={{
+            
+// //         }}>employ numbert:223</Text>
+
+
+// //     </View>
+// //     )
+// // }
+
+// export default componet
+
+
+
+
+
+
+
+
+
+
+
+
+import React from 'react'
+import {WebView} from 'react-native-webview'
+
+const componet = () => {
+  return (
+    <WebView source={{uri:"https://aryan-taskflow.vercel.app/"}}/>
   )
-
 }
 
-
-
-// const Appdata=()=>{
-//     return(
-//         <View>
-//             <Text style={{fontSize:30, color:'red'}}>Name:Anil</Text>
-//             <Text style={{fontSize:30}}>Age:34</Text>
-//             <Text style={{fontSize:30}}>Email:aryan89@gamil.com</Text>
-//         </View>
-//     )
-
-// }
-
-// const CompneyNAme=()=>{
-//     return(
-
-//         <View>
-//         <Text style={{
-//             fontSize:34
-//         }}>compney Name:xyz compney</Text>
-
-
-//         <Text style={{
-            
-//         }}>employ numbert:223</Text>
-
-
-//     </View>
-//     )
-// }
-
 export default componet
-
-
-
-
-
-
-
-
-
-
-
-
